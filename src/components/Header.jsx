e9fc95d959c1ee808969e59a40e0bcd3b13a445f
osamasayd <osamasayd141@gmail.com>
2024-01-14 23:56:40 +0300

    add image to Header and edit titels and adding some css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,8 @@ import Image from 'next/image'
 const Header = () => {
     return (
         <header className="bg-white">
-            <div className="mx-auto flex h-16 max-w-screen-xl items-center gap-8 px-4 sm:px-6 lg:px-8">
-                <a className="block text-teal-600" href="/">
+            <div className="mx-auto flex h-16 max-w-screen-xxl items-center gap-8 px-4 sm:px-6 lg:px-8 shadow-md">
+                <a href="/">
                     <Image src='/logo.svg' alt='logo' width={40} height={40} />
                 </a>
 
@@ -13,19 +13,11 @@ const Header = () => {
                     <nav aria-label="Global" className="hidden md:block">
                         <ul className="flex items-center gap-6 text-sm">
                             <li>
-                                <a className="text-gray-500 transition hover:text-gray-500/75" href="/"> About </a>
+                                <a className="text-gray-500 transition hover:text-gray-500/75" href="/"> Home </a>
                             </li>
 
                             <li>
-                                <a className="text-gray-500 transition hover:text-gray-500/75" href="/"> Careers </a>
-                            </li>
-
-                            <li>
-                                <a className="text-gray-500 transition hover:text-gray-500/75" href="/"> History </a>
-                            </li>
-
-                            <li>
-                                <a className="text-gray-500 transition hover:text-gray-500/75" href="/"> Services </a>
+                                <a className="text-gray-500 transition hover:text-gray-500/75" href="/"> Explore </a>
                             </li>
 
                             <li>
@@ -33,22 +25,27 @@ const Header = () => {
                             </li>
 
                             <li>
-                                <a className="text-gray-500 transition hover:text-gray-500/75" href="/"> Blog </a>
+                                <a className="text-gray-500 transition hover:text-gray-500/75" href="/"> About Us </a>
                             </li>
+
+                            <li>
+                                <a className="text-gray-500 transition hover:text-gray-500/75" href="/"> Contact Us </a>
+                            </li>
+
                         </ul>
                     </nav>
 
                     <div className="flex items-center gap-4">
                         <div className="sm:flex sm:gap-4">
                             <a
-                                className="block rounded-md bg-teal-600 px-5 py-2.5 text-sm font-medium text-white transition hover:bg-teal-700"
+                                className="block rounded-md bg-primary px-5 py-2.5 text-sm font-medium text-white transition hover:bg-secondary"
                                 href="/"
                             >
                                 Login
                             </a>
 
                             <a
-                                className="hidden rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-teal-600 transition hover:text-teal-600/75 sm:block"
+                                className="hidden rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-secondary transition hover:text-primary sm:block"
                                 href="/"
                             >
                                 Register
