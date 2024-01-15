f8f678a929e06bd2e0113e8befa89891f87d82b8
osamasayd <osamasayd141@gmail.com>
2024-01-15 17:28:47 +0300

    edit primary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,7 @@ module.exports = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
       colors:{
-        primary: '#27005D',
+        primary: '#5D12D2',
         secondary: '#5B0888'
       }
     },
