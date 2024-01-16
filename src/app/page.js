1ee5181bb19d5837b9f470fd89c5823594064fe7
osamasayd <osamasayd141@gmail.com>
2024-01-16 05:07:03 +0300

    add ProductSection

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Hero from '@/components/Hero'
+import ProductSection from '@/components/ProductSection'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <div>
       <Hero />
+      <ProductSection />
     </div>
   )
 }
