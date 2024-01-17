41f375052a0cc5e19a5df74a82c77d10ead8351f
osamasayd <osamasayd141@gmail.com>
2024-01-18 01:44:02 +0300

    create ProductApis

--- src/utils/ProductApis.js
+++ src/utils/ProductApis.js
@@ -1,7 +1,11 @@
 const { default: axiosClient } = require("./axiosClint");
 
 const getLatestProducts = ()=>axiosClient.get('/Products?populate=*')
+const getProductById = (id)=>axiosClient.get(`/products/${id}?populate=*`)
+const getProductsByCategory = (category)=>axiosClient.get(`/products?filters[category][$eq]=${category}&populate=*`)
 
 export default {
-    getLatestProducts
+    getLatestProducts,
+    getProductById,
+    getProductsByCategory
 }
