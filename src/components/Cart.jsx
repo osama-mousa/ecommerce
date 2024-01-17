8fda7a3332420f91df1646623998cb53960f69d5
osamasayd <osamasayd141@gmail.com>
2024-01-18 01:55:05 +0300

    create Cart

---
+++ src/components/Cart.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Cart = () => {
+  return (
+    <div className='h-[300px] w-[250px] bg-gray-100 z-10 rounded-md border shadow-sm absolute mx-10 right-10 top-12 p-5 overflow-auto'></div>
+  )
+}
+
+export default Cart
