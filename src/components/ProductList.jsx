abfcf017ebbda1cf95d62385b304bdfad4899e3f
osamasayd <osamasayd141@gmail.com>
2024-01-18 01:45:09 +0300

    create ProductList

---
+++ src/components/ProductList.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ProductItem from './ProductItem'
+
+const ProductList = ({ productList }) => {
+  return (
+    <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-3'>
+      {productList.map(item => (
+        <ProductItem product={item} key={item.id} />
+      ))}
+    </div>
+  )
+}
+
+export default ProductList
