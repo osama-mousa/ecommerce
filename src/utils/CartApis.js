3be450ecbfcaf5d649550d5f54db1853ac4a12bf
osamasayd <osamasayd141@gmail.com>
2024-01-18 17:56:51 +0300

    create deleteCartItem Api

--- src/utils/CartApis.js
+++ src/utils/CartApis.js
@@ -2,10 +2,11 @@ const { default: axiosClient } = require("./axiosClint");
 
 const addTocart = (payload)=>axiosClient.post('/carts', payload)
 const getUserCartItems = (email)=>axiosClient.get(`/carts?populate[products][populate]=banner&filters[email][$eq]=${email}`)
-
+const deleteCartItem = (id)=>axiosClient.delete(`/carts/${id}`)
 
 export default {
     addTocart,
     getUserCartItems,
+    deleteCartItem,
 
 }
