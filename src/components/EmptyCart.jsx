42786028d2bdd70165fbfbb7dbc4821830d4fba9
osamasayd <osamasayd141@gmail.com>
2024-01-22 01:48:35 +0300

    update EmptyCart

--- src/components/EmptyCart.jsx
+++ src/components/EmptyCart.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { PiBatteryWarningVertical } from "react-icons/pi";
 
-const EmptyCart = ({size}) => {
+const EmptyCart = ({ size }) => {
     return (
-        <div className='text-gray-400 text-center my-5'>
+        <div className='text-gray-400 flex flex-col items-center justify-center my-5'>
             <h2 className='text-lg'>Cart Empty</h2>
             <div className=''>
-                <PiBatteryWarningVertical className={`items-center text-center my-5 size-${size}`} />
+                <PiBatteryWarningVertical className={`my-5 size-${size}`} />
             </div>
         </div>
 
